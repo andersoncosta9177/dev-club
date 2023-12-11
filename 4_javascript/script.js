095715766a0684ea14a8375cd55d727a074fc635
Anderson miguel costa <anderson_costa_miguel@hotmail.com>
2023-12-11 12:41:16 -0300

    aula js basico

---
+++ 4_javascript/script.js
@@ -0,0 +1,557 @@
+// console.log("ola mundo, estou aqui")
+
+// var  number = 12;
+// var condition = true
+// var name = 'anderson'
+// var lastname = 'costa'
+
+
+// var number1 = 1
+// let number2 = 2
+// const number3 = 3
+
+// console.log(number1)
+// console.log(number2)
+// console.log(number3)
+// console.log(`${name} ${lastname}`)
+
+//permitido
+// camelCase 
+// snake_case
+//iniciar com letras
+//letras maiusculas e minusculas
+
+
+// tipos de dados
+//string em javascript
+
+// let myFirstSring = 'string com aspas simples';
+// let mySecondString =  "String com aspas duplas"
+// let myThirdString = `string com templates strings`
+// let usedAspas = 'pode usar "aspas duplas" dentro de uma aspas simples'
+//  console.log(usedAspas)
+
+//  let numbersOfPeoples = 34
+//  console.log(`havia ${numbersOfPeoples} de pessoas na sala`)
+
+//tipos de dados: => numbers
+// const numerOne = 234  //numbers
+// const numnerTwo = '234'  //string
+
+// const n1 = 234
+// const n2 = 321;
+
+// const resultPlus = n1 + n2
+// console.log(resultPlus)
+
+// const resultDivide = n2 /  n1
+// console.log(resultDivide.toFixed(2))
+
+
+// const resultMultiplication = n2 *  n1
+// console.log(resultMultiplication)
+
+// Object em javascript
+
+//    const person = {
+//     nome: 'Anderson',
+//     lastname: 'costa',
+//     idade: 33,
+//     sexo: 'Male',
+
+//     address: {
+//         rua: 'rua 6',
+//         bairro: 'setor 9',
+//         cep: '7292-822'
+//     }
+
+//    }
+//    console.log(person)
+// // console.log(person.nome)
+// console.log(person.address)
+
+
+
+// BOOLEANOS EM JAVASCRIPT
+
+
+//   const conditionMedical = true
+//   const older_age =  true;
+
+//   if(conditionMedical === true && older_age === true){
+//     console.log('Aprovado na aptidão fisica')
+//   }else{
+//     console.log("Não aprovado na aptidao fisica")
+//   }
+
+
+// array em javascript
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(numbers)
+
+// const arrays = [45, 'anderson', false, true, {nome: 'anderson', age:33}]
+
+// const peoples = ['alan','Anderson','Augusto','Miguel']
+// console.log(peoples)
+
+
+// console.log(arrays)
+
+
+
+// math em javascript
+
+// console.log(Math.PI)
+// console.log(Math.pow(5,2))
+// console.log(Math.sqrt(25))
+// console.log(Math.floor(Math.random()* 100))
+// console.log(Math.ceil(Math.random()* 100))
+// console.log(Math)
+
+
+// const n1 = 23
+// const n2 = 33
+// console.log(n1 + n2)
+// =================================
+// const n1 = 23
+// const n2 = 33
+// console.log(n1 * n2)
+// =================================
+
+// const n1 = 25
+// const result = Math.sqrt(n1) 
+
+// console.log(result * 10 / 33)
+
+// console.log(result * 10)
+// console.log(result * 10 /  33)
+//===================================
+//    const nome = 'Anderson'
+//    const name_son = 'Miguel'
+//    console.log(`ola, meu nome é ${nome} e o nome do meu filho é ${name_son}`)
+//    //====================================================
+// const nome = 'Miguel'
+// const name_son = 'Miguel'
+
+// console.log(nome === name_son)
+
+// if (nome === name_son) {
+//     console.log('os nomes são iguais')
+
+// } else {
+//     console.log('os nomes não sao iguais')
+// }
+//    //====================================================
+//    const names = ['Miguel','Anderson']
+//   console.log(names[0] ===  names[1])
+
+//   const names = ['Miguel','Miguel']
+//   console.log(names[0] ===  names[1])
+//    //====================================================
+
+// const person1 = {
+//     nome: 'Vitor',
+//     age: 23,
+//     sexo: 'male',
+//     city: 'Aguas claras',
+// }
+// const person2 = {
+//     nome: 'Leandro',
+//     age: 43,
+//     sexo: 'male',
+//     city: 'Sao luiz',
+// }
+// const person3 = {
+//     nome: 'Maria',
+//     age: 34,
+//     sexo: 'fem',
+//     city: 'Aguas lindas',
+// }
+// const person4 = {
+//     nome: 'Miguel',
+//     age: 10,
+//     sexo: 'male',
+//     city: 'Aguas lindas',
+// }
+
+// const person5 = {
+//     nome: 'Alan',
+//     age: 33,
+//     sexo: 'male',
+//     city: 'Vila rica',
+// }
+
+
+
+//    //====================================================
+// const friend2 = {
+//     nome: 'Vitor',
+//     age: 23,
+//     sexo: 'male',
+//     city: 'Aguas claras',
+// }
+// const friend1 = {
+//     nome: 'Leandro',
+//     age: 43,
+//     sexo: 'male',
+//     city: 'Sao luiz',
+// }
+// console.log(friend1.nome === friend2.nome)
+
+//======================================
+
+// const numberRandom = Math.floor(Math.random()* 100) 
+// console.log(numberRandom)
+// const numberRandom = Math.ceil(Math.random() * 100)
+// console.log(numberRandom)
+
+//===============================================
+
+// const n1 = 25
+// const n2 = 5
+// const result =  n1 % n2
+// console.log(result)
+//======================
+
+// const n3 = 12
+// const n4 = 33
+// console.log(n3 > n4)
+// console.log(n3 >= n4)
+// console.log(n3 < n4)
+// console.log(n3 <= n4)
+
+//==========================================
+
+//    const names = ['Miguel','Anderson', 'Vanda', 'Augusto', true]
+// console.log(names.length)
+
+//=============================================
+
+//operadortes aritimeticos em js
+
+// let myNumber = 10
+// // myNumber--
+// // myNumber--
+// // myNumber--
+// // myNumber++
+// console.log(++myNumber)
+// console.log(myNumber)
+// let firstNumber = 10
+// firstNumber += 2
+// console.log(firstNumber)
+
+
+// let firstNumber = 10
+// firstNumber -= 2
+// console.log(firstNumber)
+
+
+// let firstNumber = 10
+// firstNumber *= 2
+// console.log(firstNumber)
+
+
+// let firstNumber = 10
+// firstNumber /= 2
+// console.log(firstNumber)
+
+
+//operadores de comparaçao
+
+// const firstPerson = 'Miguel'
+// const secondPerson = 'Anderson'
+// console.log(firstPerson == secondPerson)
+
+// const n1 = 12
+// const n2 = '12'
+// console.log(n1 == n2)
+// console.log(n1 === n2)
+
+// const firstPerson = 'Miguel'
+// const secondPerson = 'Miguel'
+// console.log(firstPerson !== secondPerson)
+
+// const firstPerson = 'Anderson'
+// const secondPerson = 'Miguel'
+// console.log(firstPerson !== secondPerson)
+
+// const n1 = 67
+// const n2 = 45
+// const n3 = 67
+
+// console.log(n1 > n2)
+// console.log(n1 < n2)
+// console.log(n3 <= n1)
+// console.log(n1 >= n3)
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(true || false)
+// console.log(!true || !false)
+// console.log( true && !false && true)
+// console.log( true && false && true)
+
+// const age = 2
+
+// const result = age> 18 ? "Pode votar":"Não pode votar"
+// console.log(result) 
+
+// const temPassaporte = false
+// const resultado = temPassaporte === true ? "Pode viajar":"Nao pode viajar"
+// console.log(resultado)
+
+
+// const rain = true
+// const umbrella = rain ? "Levar guarda chuva" : "Nao levar guarda chuva"
+// console.log(umbrella)
+
+// const balance = true
+// const isNotBlocked =  true
+// const accountExist = false
+// const transferOk = balance && isNotBlocked && accountExist ? "Transferencia realizada":"ERRO: verifique os dados e tente novamente"
+// console.log(transferOk)
+
+
+//=========operadores typeof e delete
+
+// const myNumber = 20
+// const myString = 'Anderson costa'
+
+
+// const person = {
+//     nome: 'Vanda francisca',
+// }
+
+// console.log(typeof myNumber)
+// console.log(typeof myString)
+// console.log(typeof person)
+
+
+// const person = {
+//     nome: 'Vanda francisca',
+//     age: 33
+
+// }
+
+// delete person.age
+// console.log(person)
+
+
+// if else em javascript
+
+// if(true){
+//     console.log('entrei aqui')
+// }
+
+// console.log('Terminou de executar o codigo')
+
+// const age = 17
+
+// if(age >= 18){
+//     console.log('Maior de idade')
+// }else{
+//     console.log("menor de idade")
+// }
+
+
+// else if em javascript
+
+// const temperature = 37
+
+// if(temperature >= 36 && temperature <= 38){
+//     console.log("A pessoa esta saudavel")
+// }else if(temperature > 39){
+//     console.log("A pessoa esta com febre")
+// }else{
+//     console.log("A pessoa esta com a temperatura muito baixa")
+// }
+
+
+//=========SWITCH -CASE=====
+// const numberWeek = 5
+// switch (numberWeek) {
+//     case 1:
+//         console.log("Domingo")
+//         break
+//     case 2:
+//         console.log("Segunda")
+//         break
+//     case 3:
+//         console.log("terça")
+//         break
+//     case 4:
+//         console.log("quarta")
+//         break
+//     case 5:
+//         console.log("quinta")
+//         break
+//     case 6:
+//         console.log("sexta")
+//         break
+//     case 7:
+//         console.log("Sabado")
+//         break
+
+
+//     default:
+//         console.log("dados incorretos")
+// }
+
+// ========for============
+
+// for(let  i =  0; i <= 10;i++){
+//     console.log(i)
+// }
+
+// for(let j =0; j <= 20; j++){
+//     console.log(j)
+// }
+
+
+// for(let j =0; j <= 20; j++){
+//     console.log(j)
+// }
+
+// const students = ['miguel', 'alan', 'andressa', 'thiago']
+// for (let i = 0; i < students.length; i++) {
+//     console.log(` '${students[i]}' esta presente na sala`)
+// }
+
+//=========while em js=======
+// let i = 6474737
+// while (i > 20) {
+//     console.log(i)
+//     i /= 40
+
+// }
+
+
+//======for of em avascript
+// let myName = 'Anderson'
+// let allNames =  ['maria', 'suzane', 'roberta']
+
+// for(let letter of myName){
+//     console.log(letter)
+// }
+
+
+// for(let nomes of allNames){
+//     console.log(nomes)
+// }
+
+
+//=====FOR IN  em js=====
+
+// const student = {
+//     name: 'caio',
+//     age: 17,
+//     genre: 'male'
+// }
+
+// for(let property in student){
+//     console.log(`${property} : ${student[property]}`)
+// }
+
+//======desafios =====
+
+// const n = 10
+
+// if(n > 10){
+//     console.log("maior  que 10")
+
+
+// }else if(n < 10){
+//     console.log("menor a 10")
+// }else{
+//     console.log("igual que 10")
+
+// }
+
+
+// const person = {
+//     nome: 'joao', 
+//     nationality: 'Brasileira'
+// }
+// if(person.nationality === "Brasileira"){
+//     console.log("Ele e brasileira")
+
+// }else{
+//     console.log("Ele é estrangeiro")
+
+// }
+
+// const number = Math.ceil(Math.random() * 10)
+// const numberWinner = Math.ceil(Math.random() * 10)
+// if( number === numberWinner){
+//     console.log("Parabens, voçe ganhou")
+// }
+// else{
+//     console.log("Voçe perdeu")
+// }
+
+
+// const n1 = 45
+// const n2 = 46
+// const n3 = 44
+
+// if(n1 > n2 && n1 > n3){
+//     console.log("O primeiro é maior")
+// }else if( n2 > n1 && n2 > n3){
+//     console.log("O segundo numero é maior")
+// }else{
+//     console.log("O terceiro numero é maior")
+// }
+
+// const person1 = { nome: 'marcos', idade: 37, sexo: 'masc', profissao: 'engenheiro', nacionalidade: 'Brasileira' }
+// const person2 = { nome: 'alan', idade: 28, sexo: 'masc', profissao: 'padeiro', nacionalidade: 'Brasileira' }
+// const person3 = { nome: 'anderson', idade: 33, sexo: 'masc', profissao: 'programador', nacionalidade: 'Brasileira' }
+// const person4 = { nome: 'maria', idade: 34, sexo: 'fem', profissao: 'empresaria', nacionalidade: 'Brasileira' }
+
+// if (person1.idade >= 18 && person1.nacionalidade === 'Brasileira') {
+//     console.log('Voçe passou no processo')
+// } else {
+//     console.log("Infelizmente não passou no processo")
+// }
+
+
+// const score = 10
+// switch (score) {
+//     case 1:
+//         console.log("Apresentaçao muito ruim")
+//         break;
+//     case 8:
+//         console.log("execelnte apresentaçao , mas pode ficar ainda melhor")
+//         break;
+//     case 7:
+//         console.log("boa apresentaçao, mas senti um certo nervosismo")
+//         break;
+//     case 10:
+//         console.log("Apresentaçao espetacular")
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const n = 17
+// if(n % 2 === 0){
+//     console.log(`o numero ${n} é par`)
+// }else if(n % 2 === 1){
+//     console.log(`o numero ${n} é impar`)
+// }
+
+// const n1 = 7
+// const n2 = 16
+// const n3 = 13
+// const n4 = 20
+
+// if(n1 % 2 === 0 && n2 % 2 ===0 && n3 % 2 ===0 && n4 % 2 ===0){
+//     console.log("todOs numeros sao pares")
+// }else if(n1 % 2 === 1 && n2 % 2 ===1 && n3 % 2 ===1 && n4 % 2 ===1){
+//     console.log("Tods os numeros sao impares")
+// }else{
+//     console.log("Os numero estao misturados entre pares e impares")
+// }
