37973c744ad126a60e86c1c83badc7ac2c6392b2
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2023-12-23 10:10:36 -0300

    update node module

--- 7_node/index.js
+++ 7_node/index.js
@@ -1,93 +1,63 @@
 const express = require("express")
-const app = express()
-const  cors  =  require("cors")
-
 const uuid = require("uuid")
+const app = express()
 app.use(express.json())
-app.use(cors())
-
 
 
 const users = []
 
 
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+    const index = users.findIndex(user => user.id === id)
+    if (index < 0) {
+        return response.status(404).json({ error: "Usuario não encontrado" })
+    }
+    request.userIndex = index
+    request.userId = id
+    next()
+}
 
 
+app.get("/users", (request, response) => {
+    // const { name, age } = request.body
+    console.log("a rota foi chamada")
+    return response.json(users)
 
-
-
-
-
-app.get('/users', (req, res) => {
-    const { nome, age } = req.body
-    return res.json({ users })
 })
 
-
-
-app.post("/users", (req, res) => {
-
-
-    try {
-        const { name, age } = req.body
+app.post("/users", (request, response) => {
+    const { name, age } = request.body
     const user = { id: uuid.v4(), name, age }
     users.push(user)
-    return res.status(201).json(user)
-        
-
-    } catch (error) {
-        return res.status(500).json({error: 'Erro interno do servidor'})
-        
-    } finally{
-        console.log("tudo corretamente!")
-    }
-    
+    return response.status(201).json(user)
 })
 
 
-
-
-
-
-
-app.put("/users/:id", (req, res) => {
-    const { name, age } = req.body
-    const { id } = req.params
-
-
-    const updatesUsers = { id, name, age }
-    const index = users.findIndex(user => user.id === id)
-
-    if (index < 0) {
-        return res.status(404).json({ mesage: "user not found" })
-    }
-    users[index] = updatesUsers
-
-    return res.json(updatesUsers)
+app.put("/users/:id", checkUserId, (request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId;
+    const updateUsers = { id, name, age }
+    users[index] = updateUsers
+    return response.json(updateUsers)
 })
 
 
-
-
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params
-
-    const index = users.findIndex(user => user.id === id)
-    if (index < 0) {
-        return res.status(404).json({ mesage: "user not found" })
-    }
-
+app.delete("/users/:id", checkUserId, (request, response) => {
+    const index = request.userIndex
     users.splice(index, 1)
+    return response.status(204).json()
+})
 
 
-
-    return res.status(204).json()
+const port = 3001
+app.listen(port, () => {
+    console.log(`server started  ativo on port: ${port}`)
 })
 
 
 
 
 
-app.listen(3001, () => {
-    console.log("Rodando na porta 1337 ✔")
-})
+
